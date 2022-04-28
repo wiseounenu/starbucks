b01697ab0dc250c4930352d57baeba5e85eaa087
wiseounenu <weeks1866@naver.com>
2022-04-28 14:24:23 +0900

    공동모듈분리

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-// 서치를 DIV를 클릭하면 포커스해라 라는 의미
-searchEl.addEventListener('click', function(){
-  //Logic..
-  searchInputEl.focus();
-  // 아무곳이나 클릭하면 효과가 적용된다
-});
-
-// 만약에 포커스가 되면 두번째로 작성하는 인수가 실행될거다 ( 핸들러 )
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  // 클래스에서 에드를 추가하겠다
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  // 클래스에서 에드를 추가하겠다
-  searchInputEl.setAttribute('placeholder','');
-});
-
 
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -164,7 +141,3 @@ spyEls.forEach(function(spyEl){
   .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-                      //자바스크립트 생성자 함수
-                      //데이터 정보가 반환된다
-thisYear.textContent = new Date().getFullYear();
